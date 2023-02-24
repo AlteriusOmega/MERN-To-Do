cf380dbd1fe2ff7824006b61f2badecb9c507eaa
AlteriusOmega <TheRealSamChaney@gmail.com>
2023-02-23 23:53:14 -0600

    Initial commit

---
+++ api/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=> console.log("Connected to DB"), ()=> console.log("Could not connect to DB!") ).catch(console.error);
+
+const Todo = require("./models/Todo");
+
+app.get("/todos", async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+    // So if we make a request to localhost:3001/todos, it will find the todos via line 20 Todo.find() using our model Todo defined in the file Todo.js which connects to our mongoose database, gets the todos we need and passes them back to this file as res
+})
+
+
+app.post("/todo/new", (req, res) => {
+        // Below is going to create a new todo collection inside our MongoDB database when we go to localhost/todo/new URL
+    const todo = new Todo({
+        // Text is the only required field in our databae. req.body means anything we pass through in our request, like the body of the request, we will get in here
+        text: req.body.text
+    });
+
+    todo.save(); // Saves to our collection in the database
+    res.json(todo); // Will pass our new todo back so we can add it to our list
+    
+})
+
+app.delete("/todo/delete/:id", async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+    res.json(result);
+})
+
+app.get("/todo/complete/:id", async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+    todo.complete = !todo.complete;
+    todo.save();
+    res.json(todo);
+})
+
+app.listen(3001, ()=> console.log("Sever started on port 3001"));
+
---
+++ client/src/App.js
@@ -0,0 +1,101 @@
+import {useState, useEffect} from 'react';
+
+// The below code 
+
+const API_BASE = "http://localhost:3001";
+
+function App(){
+	// Below, the useState is a "hook" or function from React that lets you make "state variables" which can change their state based on certain events which triggers them to re-render. useState takes in one value, and returns an array with two things in it. The first is the current value of the state variable, and the second is the function that is used to update that variable. So, what you see below is us setting these arrays of variables using useState. The first item in the array on the left side is the name of the state variable, and the second one is the name of the function it will use to update that variable.
+	const [todos, setTodos] = useState([]); // This empty array is where out todos will be stored
+	const [popupActive, setPopupActive] = useState(false);
+	const [newTodo, setNewTodo] = useState("");
+
+
+	useEffect(() => {
+		GetTodos();
+		console.log(todos);
+	
+	}, []) // Here we are passing an empty array, so this once it loads and the component mounts
+
+	const GetTodos = () => {
+		fetch(API_BASE + "/todos")
+			.then(res => res.json())
+			.then(data => setTodos(data))
+			.catch(err => console.error("Error: ", err));
+	}
+
+	const completeTodo = async id => {
+		const data = await fetch(API_BASE + "/todo/complete/" + id)
+			.then(res => res.json());
+
+		setTodos(todos => todos.map(todo => {
+			if (todo._id === data._id){
+				todo.complete = data.complete;
+			}
+			return todo;
+		}))
+	}
+
+	const deleteTodo = async id => {
+		const data = await fetch(API_BASE + "/todo/delete/" + id, {method: "DELETE"})
+			.then(res => res.json());
+				// Our todo should not be equal  
+			setTodos(todos => todos.filter(todo => todo._id !== data._id));
+	}
+
+	const addTodo = async () => {
+		const data = await fetch(API_BASE + "/todo/new", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json"
+			},
+			body: JSON.stringify({
+				text:newTodo
+			})
+			}).then(res => res.json());
+			setTodos([...todos, data]);
+			setPopupActive(false);
+			setNewTodo("");
+	}
+
+	return (
+		<div className="App">
+			<h1>Welcome, Sam!</h1>
+			<h4>My TODOs</h4>
+
+			<div className="todos">
+				{/* Below, map runs a function on every item in todos, and that function is our arrow/lambda function which displays the HTML divs, and dynamically fills in the text of each todo, and even dynamically updates the classes in className to reflect whether its complete or not */}
+				{todos.map(todo =>(
+					<div className={"todo "+(todo.complete? "is-complete": "")} key={todo._id} onClick={()=> completeTodo(todo._id)}>
+						<div className="checkbox"></div>
+						<div className="text">{todo.text}</div>
+						<div className="delete-todo" onClick={()=> deleteTodo(todo._id)}>X</div>
+					</div>
+				))}
+			</div>
+
+			<div className="addPopup" onClick={()=> setPopupActive(true)}>+</div>
+
+			{/* Below, we're checking if the popup is active by checking the bool of popupActive. If it is we create all these HTML divs which will be the make new popup */}
+			{popupActive ? (
+				<div className="popup">
+					<div className="closePopup" onClick={()=> setPopupActive(false)}>X</div>
+					<div className="content">
+						<h3>Add Task</h3>
+						{newTodo}
+						<input 
+						type="text"
+						className="add-todo-input" 
+						onChange={e => setNewTodo(e.target.value)}
+						value={newTodo}></input>
+					<div className="button" onClick={addTodo}>Create Task</div>
+					</div>
+
+				</div>
+			): ""}
+
+		</div>
+	);
+}
+
+export default App;
